4f0de013f6c73f730a0ec5683341c03b75833ee3
Loïc Wolff <loicwolff@users.noreply.github.com>
2009-02-22 12:07:31 +0100

    updated MainFrame.py now allow to chose which files are generated added .icns file

--- gui/MainFrame.py
+++ gui/MainFrame.py
@@ -2,62 +2,105 @@
 # -*- coding: utf-8 -*-
 
 import wx
-from tool.SubtitleLibOld import SubtitleFile
+import sys
+if sys.version_info[0:2] == (2, 6) or sys.version_info[0] == 3:
+  print("importing py3k-ready version")
+  from tool.SubtitleLib import SubtitleFile
+else:
+  print "import py2.5 legacy version"
+  from tool.SubtitleLibOld import SubtitleFile
 
 class MainFrame(wx.Frame):
   """This is the main frame of the application.
   It should be designed to be as simple as possible.
   """
   
-  MAIN_FRAME_ID = wx.NewId()
-  DROPBOX_ID = wx.NewId()
-  DROP_TEXT_ID = wx.NewId()
+  _MAIN_FRAME_ID = wx.NewId()
+  _TO_ASS_CHECKBOX_ID = wx.NewId()
+  _REMOVE_TAG_CHECKBOX_ID = wx.NewId()
+  _TO_TRANSCRIPT_CHECKBOX_ID = wx.NewId()
+  
+  _to_ass_checkbox = None
+  _remove_tag_checkbox = None
+  _to_transcript_checkbox = None
     
   def __init__(self):
     wx.Frame.__init__(self, None, -1, u"NoTagApp", wx.DefaultPosition, wx.Size(400, 300), 
-                        wx.CLOSE_BOX | wx.MINIMIZE_BOX | wx.CAPTION | wx.STAY_ON_TOP, u"MainFrame")
+                        wx.CLOSE_BOX | wx.MINIMIZE_BOX | wx.CAPTION | wx.STAY_ON_TOP)
     
     wx.InitAllImageHandlers()
     self.initControls()
+    
+    self._to_transcript_checkbox.SetFocus()
+    self.SetMinSize(wx.Size(-1, 100))
+    self.SetMaxSize(wx.Size(-1, 100))
+
+    self.Center()
+    self.Show(True)
 
   def initControls(self):
     """create and initialize UI elements"""
     
     # Controls Initialization and showing of the form
-    mainpanel = wx.Panel(self, self.MAIN_FRAME_ID)
-    s_box = wx.StaticBox(mainpanel)
+    mainpanel = wx.Panel(self, self._MAIN_FRAME_ID)
     
-    drop_text = wx.StaticText(mainpanel, self.DROP_TEXT_ID, u"Drop file(s) here", 
-                                      wx.DefaultPosition, wx.DefaultSize, wx.CENTRE)    
+    drop_text = wx.StaticText(mainpanel, wx.ID_ANY, u"Drop file(s) here")
+    drop_text.SetWindowStyle(wx.CENTRE)
     drop_text.SetFont(wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
     
-    static_sizer = wx.StaticBoxSizer(s_box, wx.VERTICAL)
-    static_sizer.Add(drop_text, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 20)
+    static_box = wx.StaticBox(mainpanel)
+    drop_sizer = wx.StaticBoxSizer(static_box, wx.VERTICAL)
+    drop_sizer.Add(drop_text, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 20)
     
-    mainpanel.SetSizer(static_sizer)
+    self._to_ass_checkbox = wx.CheckBox(mainpanel, self._TO_ASS_CHECKBOX_ID, u"Generate .ass")
+    self._remove_tag_checkbox = wx.CheckBox(mainpanel, self._REMOVE_TAG_CHECKBOX_ID, u"Remove tags")
+    self._to_transcript_checkbox = wx.CheckBox(mainpanel, self._TO_TRANSCRIPT_CHECKBOX_ID, u"Transcript")
     
-    target1 = DropFile()
-    s_box.SetDropTarget(target1)
-    target2 = DropFile()
-    drop_text.SetDropTarget(target2)
+    cb_sizer = wx.BoxSizer(wx.HORIZONTAL)
+    cb_sizer.Add(self._to_transcript_checkbox, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 20)
+    cb_sizer.AddSpacer(20)
+    cb_sizer.Add(self._remove_tag_checkbox, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 20)
+    cb_sizer.AddSpacer(20)
+    cb_sizer.Add(self._to_ass_checkbox, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 20)
+    
+    self._to_transcript_checkbox.SetValue(False)
+    self._remove_tag_checkbox.SetValue(True)
+    self._to_ass_checkbox.SetValue(True)
 
-    self.SetMinSize(wx.Size(320, 100))
-    self.SetMaxSize(wx.Size(320, 100))
+    main_sizer = wx.BoxSizer(wx.VERTICAL)
+    main_sizer.Add(drop_sizer, 1, wx.EXPAND, 10)
+    main_sizer.Add(cb_sizer, 0, wx.ALIGN_CENTER_HORIZONTAL, 0)
 
-    self.Center()
-    self.Show(True)
-
+    mainpanel.SetSizer(main_sizer)
+    
+    static_box_target = DropFile(self)
+    static_box.SetDropTarget(static_box_target)
+    
+    drop_text_target = DropFile(self)
+    drop_text.SetDropTarget(drop_text_target)
+    
+  def getGenerateFiles(self):
+    """Return a tuble of the checkboxes values"""
+    return (self._to_transcript_checkbox.IsChecked(), 
+            self._remove_tag_checkbox.IsChecked(),
+            self._to_ass_checkbox.IsChecked())
 
 class DropFile(wx.FileDropTarget):
   """"""
-    
-  def __init__(self):
+  
+  def __init__(self, parent):
     wx.FileDropTarget.__init__(self)
+    self._parent = parent
         
   def OnDropFiles(self, x, y, files):
+    _to_transcript, _remove_tag, _to_ass = self._parent.getGenerateFiles()
+  
     srt = SubtitleFile()
     for file in files:
       srt.File = file
-      srt.toAss()
-      srt.toTranscript()
-      srt.removeTag()
+      if _to_ass:
+        srt.toAss()
+      if _to_transcript:
+        srt.toTranscript()
+      if _remove_tag:
+        srt.removeTag()

85e6d1d05eb84a05dde47eb7aa9bef3614a47d20
Loïc Wolff <loicwolff@users.noreply.github.com>
2009-02-23 17:18:09 +0100

    subtitle lib now generate correct srt and ass files

--- util/subtitle.py
+++ util/subtitle.py
@@ -4,12 +4,30 @@
 from __future__ import with_statement
 import re
 
-class SubtitleType(object):
-  SRT = 1
-  ASS = 2
+def removeExoticChar(entry):
+  """remove any exotic character from the text to make it more compatible"""
+  no_tag_pattern = { "œ":"oe", "Œ":"Oe", "Æ":"Ae", "æ":"ae" }
+  for key, value in no_tag_pattern.items():
+    entry = re.sub(key, value, entry)
+  return entry
   
-  def __init__(self):
-    pass
+def removeTag(entry, keep_italics):
+  """remove SRT and ASS tags
+  if <code>keep_italics>, italics tags are not removed
+  """
+  tag_pattern = "{.*?}|</?font.*?>|</?u>|</?b>" if keep_italics else "{.*?}|</?font.*?>|</?u>|</?b>|</?i>"
+  return re.sub(tag_pattern, "", entry)
+  
+def toAssPattern(entry):
+  """change the SRT tags into ASS tags"""
+  to_ass_pattern = { "<i>":"{\i1}", "</i>":"{\i0}", "<u>":"{\u1}", "</u>":"{\u0}", "<b>":"{\b1}", "</b>":"{\b0}" }
+  for key, value in to_ass_pattern.items():
+    entry = re.sub(key, value, entry) 
+  return entry
+
+# filetype constants
+SRT_FILE = 1
+ASS_FILE = 2
 
 class SubtitleFile(object):
   _subs = []
@@ -21,8 +39,7 @@ class SubtitleFile(object):
   def __init__(self, file=None):
     if file is not None:
       self._setFile(file)
-   
-  
+    
   def _parseSRT(self):
     """"""
         
@@ -64,33 +81,33 @@ class SubtitleFile(object):
 
   def toAss(self, keep_tag):
     """"""
-    output_file = open("%s.%s.ass" % (self._sub_name, "TAG" if keep_tag else "NOTAG"), "w")
-    
-    output_file.write("[Script Info]\r\n")
-    output_file.write("Title: <untitled>\r\n")
-    output_file.write("Original Script: <unknown>\r\n")
-    output_file.write("ScriptType: v4.00+\r\n")
-    output_file.write("PlayResX: 384\r\n")
-    output_file.write("PlayResY: 288\r\n")
-    output_file.write("PlayDepth: 0\r\n")
-    output_file.write("Timer: 100.0\r\n")
-    output_file.write("WrapStyle: 0\r\n")
-    output_file.write("\r\n")
-    output_file.write("[v4+ Styles]\r\n")
-    output_file.write("Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, ")
-    output_file.write("Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, ")
-    output_file.write("Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\r\n")
-    output_file.write("Style: Default,Arial,20,&H00FFFFFF,&H00000000,&H00000000,&H00000000,")
-    output_file.write("0,0,0,0,100,100,0,0,1,2,0,2,15,15,15,0\r\n")
-    output_file.write("\r\n")
-    output_file.write("[Events]\r\n")
-    output_file.write("Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\r\n")
-
     with open("%s.%s.ass" % (self._sub_name, "TAG" if keep_tag else "NOTAG"), "w") as output_file:
+      output_file = open("%s.%s.ass" % (self._sub_name, "TAG" if keep_tag else "NOTAG"), "w")
+      
+      output_file.write("[Script Info]\r\n")
+      output_file.write("Title: <untitled>\r\n")
+      output_file.write("Original Script: <unknown>\r\n")
+      output_file.write("ScriptType: v4.00+\r\n")
+      output_file.write("PlayResX: 384\r\n")
+      output_file.write("PlayResY: 288\r\n")
+      output_file.write("PlayDepth: 0\r\n")
+      output_file.write("Timer: 100.0\r\n")
+      output_file.write("WrapStyle: 0\r\n")
+      output_file.write("\r\n")
+      output_file.write("[v4+ Styles]\r\n")
+      output_file.write("Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, ")
+      output_file.write("Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, ")
+      output_file.write("Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\r\n")
+      output_file.write("Style: Default,Arial,20,&H00FFFFFF,&H00000000,&H00000000,&H00000000,")
+      output_file.write("0,0,0,0,100,100,0,0,1,2,0,2,15,15,15,0\r\n")
+      output_file.write("\r\n")
+      output_file.write("[Events]\r\n")
+      output_file.write("Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\r\n")
+
       for sub in self._subs:
         output_file.write("Dialogue: 0,%s,%s,Default,,0000,0000,0000,,%s%s\r\n" % (
-                            sub.StartTime,
-                            sub.EndTime,
+                            sub.StartTime.toAss(),
+                            sub.EndTime.toAss(),
                             toAssPattern(sub.FirstLine),
                             "" if sub.SecondLine == "" else "\N%s" % (toAssPattern(sub.SecondLine))))
     
@@ -107,17 +124,18 @@ class SubtitleFile(object):
     """"""
     with open("%s.%s.srt" % (self._sub_name, "TAG" if keep_tag else "NOTAG"), "w") as output_file:
       for sub in self._subs:
-        output_file.write("%s --> %s\r\n%s%s\r\n" % (
-          sub.StartTime,
-          sub.EndTime,
+        output_file.write("%d\r\n%s --> %s\r\n%s%s\r\n\r\n" % (
+          sub.Index,
+          sub.StartTime.toSRT(),
+          sub.EndTime.toSRT(),
           sub.FirstLine if keep_tag else removeTag(sub.FirstLine, True),
           "" if sub.SecondLine == "" else "\r\n%s" % (sub.SecondLine if keep_tag else removeTag(sub.SecondLine, True)))) 
 
   def _isSubtitle(self, file):
     if file[-4:] == u".srt":
-      self._type = SubtitleType.SRT
+      self._type = SRT_FILE
     elif file[-4:] == u".ass":
-      self._type = SubtitleType.ASS
+      self._type = ASS_FILE
     else:
       return False
     return True
@@ -137,7 +155,7 @@ class SubtitleFile(object):
       self._file = file
       self._sub_name = re.match(re.compile(self._sub_name_pattern), file).group(1)
       self._subs = []
-      if self._type == Subtitle.SRT:
+      if self._type == SRT_FILE:
         self._parseSRT()
       else:
         pass
@@ -156,13 +174,13 @@ class Subtitle(object):
   _second_line = ""
   _type = None
   
-  def __init__(self, type = SubtitleType.SRT):
+  def __init__(self, type = SRT_FILE):
     self._start_time = Timing()
     self._end_time = Timing()
     self._type = type
   
   def __str__(self):
-    if self._type == SubtitleType.SRT:
+    if self._type == SRT_FILE:
       return "%s --> %s\r\n%s%s" % (
           self._start_time, 
           self._end_time, 
@@ -192,7 +210,7 @@ class Subtitle(object):
     return self._second_line
           
   def _getLines(self):
-    if self._type == SubtitleType.SRT:
+    if self._type == SRT_FILE:
       return "%s\r\n%s" % (self._first_line, self._second_line)
     else:
       return "%s\N%s" % (self._first_line, self._second_line)
@@ -230,7 +248,7 @@ class Timing(object):
   _sec = 0
   _min = 0
   _hour = 0
-  _type = SubtitleType.SRT
+  _type = SRT_FILE
   
   @staticmethod
   def parseSRT(timing):
@@ -246,30 +264,31 @@ class Timing(object):
     return (Timing(m.group(1), m.group(2), m.group(3), m.group(4)), 
             Timing(m.group(5), m.group(6), m.group(7), m.group(8)))
       
-  def __init__(self, hour = 0, min = 0, sec = 0, millis = 0, type = SubtitleType.SRT):
+  def __init__(self, hour = 0, min = 0, sec = 0, millis = 0, type = SRT_FILE):
     self._millis = int(millis)
     self._sec = int(sec)
     self._min = int(min)
     self._hour = int(hour)
-    self._type = SubtitleType.SRT
+    self._type = type
       
   def toAss(self):
-    return "%s:%s:%s.%s" % (
-          str(int(self._hour)),
+    return "%d:%.2d:%.2d.%.2d" % (
+          self._hour,
           self._min, 
           self._sec, 
-          self._millis[0:2])
+          int(str(self._millis)[0:2]))
+  
+  def toSRT(self):
+    return "%.2d:%.2d:%.2d,%.3d" % (
+          self._hour,
+          self._min, 
+          self._sec, 
+          self._millis)
       
   def __str__(self):
-    if self._type == SubtitleType.SRT:
-      return "%.2d:%.2d:%.2d,%.3d" % (self._hour, self._min, self._sec, self._millis)
-    else:# self._type == SubtitleType.ASS:
-      return "%d:%.2d:%.2d.%.2d" % (
-              self._hour,
-              self._min, 
-              self._sec, 
-              self._millis)    
-      
+    if self._type == SRT_FILE:
+      return "hour: %d\nmin: %d\nsec: %d\nmillis: %d" % (self._hour, self._min, self._sec, self._millis)
+
   # getters
   def _getMillis(self):
     return self._millis
@@ -307,37 +326,12 @@ class Timing(object):
   Hour = property(_getHour, _setHour)
   Time = property(_getTime)
 
-
-def removeExoticChar(entry):
-  """remove any exotic character from the text to make it more compatible"""
-  noTagPattern = { "œ":"oe", "Œ":"Oe", "Æ":"Ae", "æ":"ae" }
-  for key, value in noTagPattern:
-    entry = re.sub(key, value, entry)
-  return entry
-  
-def removeTag(entry, keep_italics):
-  """remove SRT and ASS tags
-  if <code>keep_italics>, italics tags are not removed
-  """
-  tag_pattern = "{.*?}|</?font.*?>|</?u>|</?b>" if keep_italics else "{.*?}|</?font.*?>|</?u>|</?b>|</?i>"
-  return re.sub(tag_pattern, "", entry)
-  
-def toAssPattern(entry):
-  """"""
-  ass_pattern = { "<i>":"{\i1}", "</i>":"{\i0}", "<u>":"{\u1}", "</u>":"{\u0}", "<b>":"{\b1}", "</b>":"{\b0}" }
-  for key, value in ass_pattern:
-    entry = re.sub(key, value, entry) 
-  return entry
-
-
-
 if __name__ == "__main__":
   s = SubtitleFile()
-  s.File = "/Users/dex/Desktop/himym.srt"
+  s.File = "/Users/dex/Desktop/lost.srt"
  
-  #s.toAss()
-  #s.toTranscript()
-  #s.removeTag()
+  s.toAss(True)
+  s.toTranscript() 
   s.toSRT(True)
   
   #for sub in s.Subs:
